9d92b75bf5e656d2dd7b6781ceacc05e57acc1d1
Aryan7701 <aryanagrawal7701@gmail.com>
2023-04-13 23:51:59 +0530

    Sqrt leetcode practice

---
+++ Sqrt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sqrt {
+    static int mySqrt(int x){
+        return x;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+
+    }
+}
