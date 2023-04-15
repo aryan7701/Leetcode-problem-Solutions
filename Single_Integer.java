4c5aea81c4496744bf7f4255611ebad458eca19b
Aryan7701 <aryanagrawal7701@gmail.com>
2023-04-15 19:00:08 +0530

    ..

--- Single_Integer.java
+++ Single_Integer.java
@@ -13,6 +13,11 @@ public class Single_Integer {
         for(int i=0;i<nums.length;i++){
             result^=nums[i];
         }
+////        System.out.println(result);
+//        int result=0;
+//        for(int i=0;i<nums.length;i++){
+//            result^=nums[i];
+//        }
         System.out.println(result);
     }
 }
