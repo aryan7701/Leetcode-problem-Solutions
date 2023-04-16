417c6b5166e65eceb7abe5ec15cadb5099b9d109
Aryan7701 <aryanagrawal7701@gmail.com>
2023-04-16 18:47:11 +0530

    Length Of Last Word leetcode solution

---
+++ LengthOfLastWord.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LengthOfLastWord {
+    public static int Lengthlastword(String s){
+        String[] sentence = s.split(" ");
+        int a= sentence.length;
+        String res= sentence[a-1];
+        return res.length();
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String s= scan.nextLine();
+        System.out.println(Lengthlastword(s));
+
+    }
+}
