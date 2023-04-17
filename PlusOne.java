cd8a8b4de606773e2dbcd13a6e60f48aab032024
Aryan7701 <aryanagrawal7701@gmail.com>
2023-04-17 12:27:36 +0530

    PlusOne Leetcode Solution

---
+++ PlusOne.java
@@ -0,0 +1,16 @@
+public class PlusOne {
+        public static int[] main(int[] digits) {
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (digits[i] < 9) {
+                    ++digits[i];
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] ans = new int[digits.length+ 1];
+            ans[0]=1;
+            return ans;
+        }
+    }
+
+
