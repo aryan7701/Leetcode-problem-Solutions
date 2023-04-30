70dfcf1587c66ea89f31afab1656da4b6b7ecd4e
Aryan7701 <aryanagrawal7701@gmail.com>
2023-04-30 09:18:58 +0530

    .

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.*;
 public class ReverseInteger {
+
         public int reverse(int x) {
             long ans = 0;
             //checking if the input digit is zero or not
@@ -9,4 +12,17 @@ public class ReverseInteger {
             }
             return (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) ? 0 : (int) ans;
         }
+    public static void main(String[] args){
+            int arr[]= {5,5,5};
+            int k=2;
+            int max= Arrays.stream(arr).max().getAsInt();
+            int res=0;
+            for(int i=0;i<k;i++){
+            res=res+max;
+            max=max+1;
+            }
+            System.out.println(res);
+        }
     }
+
+
