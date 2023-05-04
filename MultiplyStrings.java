29d6d23b88c3373b022eb2edefa35c5d6e9b9b47
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-04 23:20:21 +0530

    Multiply String leetcode problem in java

---
+++ MultiplyStrings.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MultiplyStrings {
+    public static String multistring(String a, String b){
+        return "yes";
+    }
+    public static void main(String[] args){
+        System.out.println("Aryan");
+    }
+}
