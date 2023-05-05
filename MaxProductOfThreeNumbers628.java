9085c922c6fadcaeb8ce84113032a209f867b554
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-05 20:55:01 +0530

    MaxPrroduct

---
+++ MaxProductOfThreeNumbers628.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MaxProductOfThreeNumbers628 {
+    public static int MaxProduct(int nums[]){
+        return 2;
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int []nums1={1,2,3,4,5,6};
+        int nums2 []={-1,-2,-3,-4};
+        int nums3 []={-1,-2,-3,-4,6,7,8,9,10};
+
+    }
+}
