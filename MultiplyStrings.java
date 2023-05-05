ed364859e2fa164842608769edc38adf4a5e830b
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-05 22:02:50 +0530

    43). Multiply String leetcode problem in java

--- MultiplyStrings.java
+++ MultiplyStrings.java
@@ -1,9 +1,14 @@
 import java.util.*;
+import java.math.BigInteger;
 public class MultiplyStrings {
-    public static String multistring(String a, String b){
-        return "yes";
-    }
     public static void main(String[] args){
-        System.out.println("Aryan");
+        Scanner sc= new Scanner(System.in);
+        String s1= sc.next();
+        String s2=sc.next();
+        BigInteger a= new BigInteger(s1);
+        BigInteger b= new BigInteger(s2);
+        BigInteger product=a.multiply(b);
+        String res= String.valueOf(product);
+        System.out.println(res);
     }
 }
