ddf282ebc7b3ee7fc0a4c2b6fde308bd558cbc0c
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-09 22:36:28 +0530

    54).Spiral Matrix leetocde solution in java

--- CountPrimes.java
+++ CountPrimes.java
@@ -1,30 +1,47 @@
 import java.util.*;
 public class CountPrimes {
-    public static boolean IsPrime(int n){
-        boolean IsItPrime= true;
-        if(n<=1){
-            IsItPrime= false;
-            return  IsItPrime;
-        }else {
-            for (int i = 2; i <= n / 2; i++) {
-                if (n % i == 0) {
-                    IsItPrime = false;
-                    break;
+//    public static boolean IsPrime(int n){
+//        boolean IsItPrime= true;
+//        if(n<=1){
+//            IsItPrime= false;
+//            return  IsItPrime;
+//        }else {
+//            for (int i = 2; i <= n / 2; i++) {
+//                if (n % i == 0) {
+//                    IsItPrime = false;
+//                    break;
+//                }
+//            }
+//            return IsItPrime;
+//        }
+//    }
+    public static int countPrimes(int n) {
+        boolean[] list = new boolean[n];
+        for (int i = 2; i*i<=n; i++) {
+            if (list[i] == false) {
+                for (int j = 2; j*i <n; j++) {
+                    list[j*i] = true;
                 }
             }
-            return IsItPrime;
         }
+        int count = 0;
+        for (int i = 2; i<n; i++) {
+            if (list[i] == false) {
+                count++;
+            }
+        }
+        return count;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        int count=0;
-        for(int i=2;i<=n;i++){
-            if(IsPrime(i)){
-                count=count+1;
-            }
-        }
-        System.out.println(IsPrime(n));
-        System.out.println(count);
+//        int count=0;
+//        for(int i=2;i<=n;i++){
+//            if(IsPrime(i)){
+//                count=count+1;
+//            }
+//        }
+        System.out.println(countPrimes(n));
+//        System.out.println(count);
     }
 }
---
+++ SpiralMatrix.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.*;
+//Method- for spirL MATRIX
+public class SpiralMatrix {
+        public static List<Integer> spiralOrder(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            List<Integer> answer = new ArrayList<>();
+            int[][] direction = {{1,0}, {0,-1}, {-1,0}, {0,1}};
+            int[][] visited = new int[m][n];
+            for(int i = 0; i < m; i++) {
+                Arrays.fill(visited[i], 0);
+            }
+            Consumer<int[]> traverse = (coord) -> {
+                int index = 3;
+                Stack<int[]> stack = new Stack<>();
+                stack.push(coord);
+                while(!stack.isEmpty()) {
+                    coord = stack.pop();
+                    if(coord[0] >= m || coord[0] < 0 || coord[1] >= n || coord[1] < 0 || visited[coord[0]][coord[1]] == 1) {
+                        continue;
+                    }
+                    answer.add(matrix[coord[0]][coord[1]]);
+                    visited[coord[0]][coord[1]] = 1;
+                    int[] coord2 = {coord[0] + direction[index][0], coord[1] + direction[index][1]};
+                    if(coord2[0] >= m || coord2[0] < 0 || coord2[1] >= n || coord2[1] < 0 || visited[coord2[0]][coord2[1]] == 1) {
+                        index = (index + 1) % 4;
+                    }
+                    coord2 = new int[]{coord[0] + direction[index][0], coord[1] + direction[index][1]};
+                    stack.push(coord2);
+                }
+            };
+            traverse.accept(new int[]{0,0});
+            return answer;
+        }
+        public static void main(String [] args){
+
+        }
+    }
+}
