27c27fe1111b67d958cdb4f02e5fbd37350a7cf3
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-14 22:48:38 +0530

    Roman to integer leetcode solution in java

---
+++ RomanToInteger.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static final int[] table = new int[256];
+    static {
+        table['I'] = 1;
+        table['V'] = 5;
+        table['X'] = 10;
+        table['L'] = 50;
+        table['C'] = 100;
+        table['D'] = 500;
+        table['M'] = 1000;
+    }
+    public static  int romanToInt(String s) {
+        int value = 0;
+        int prev = 0;
+        int current;
+        for (char c : s.toCharArray()) {
+            current = table[c];
+            if (prev < current) {
+                value -= prev + prev;
+            }
+            value += current;
+            prev = current;
+        }
+        return value;
+
+
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(romanToInt(s));
+    }
+}
