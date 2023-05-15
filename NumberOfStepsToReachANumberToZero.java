1fd6037d0e4675eb8d8759fdae42e12a5653a4cc
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-15 11:07:33 +0530

    NumberOfStepsToReachANumberToZero leetcode problem solutioan in java

---
+++ NumberOfStepsToReachANumberToZero.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NumberOfStepsToReachANumberToZero {
+
+        public static int zero(int num){
+            int step=0;
+
+            while(num>0){
+                if(num%2==0){
+                    num =num/2;
+                    step=step+1;
+
+                }
+                else if(num%2!=0){
+                    num=num-1;
+                    step=step+1;
+                }
+            }
+
+            return step;
+        }
+        public static int numberOfSteps(int num) {
+            int res= zero(num);
+            return res;
+        }
+
+    public static void main(String [] args){
+        Scanner SC = new Scanner(System.in);
+        int num = SC.nextInt();
+        int ans= numberOfSteps(num);
+        System.out.println(ans);
+        
+    }
+}
