5d2ade7806570cdc3fd295df4b4c068466e1a214
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-16 20:58:29 +0530

    SwapNodesInPairs leetcode daily problem Solution in java

---
+++ SwapNodesInPairs.java
@@ -0,0 +1,20 @@
+
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode temNode = head;
+        while (temNode != null) {
+            list.add(temNode);
+            temNode = temNode.next;
+        }
+        for (int i = 0; i < list.size() - 1; i += 2) {
+            int swapElement = list.get(i).val;
+            list.get(i).val = list.get(i + 1).val;
+            list.get(i + 1).val = swapElement;
+        }
+        return head;
+    }
+    public static void main(String[] args){
+
+    }
+}
