b1506d24f0f87e4e3b1dcd7c52617ff0744bda95
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-18 20:57:31 +0530

    1557). findSmallestSetOfVertices leetcode daily problem solution in java

---
+++ findSmallestSetOfVertices.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+public class findSmallestSetOfVertices {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] indegree = new int[n];
+
+        for(List<Integer> list: edges){
+            indegree[list.get(1)]++;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0){
+                list.add(i);
+            }
+        }
+        return list;
+
+    }
+    public static void main(String[] args){
+
+    }
+
+}
